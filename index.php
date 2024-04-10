87460936045c41622069cc24477530de8c0b39ec
Kilian Evang <noreply@texttheater.net>
2024-04-11 00:42:55 +0200

    Added GitHub ribbon

--- index.php
+++ index.php
@@ -16,3 +16,4 @@ $l = $last[array_rand($last, 1)];
 <title>Space Karen</title>
 <link rel ="stylesheet" type="text/css" href="style.css">
 <p><?= $f ?> <?= $l ?></p>
+<a href="https://github.com/texttheater/spacekaren.texttheater.net"><img style="position: absolute; top: 0; right: 0; border: 0;" src="forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
